1a3d5c132455f405974c3c2b0c3d3f85340e31e2
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-09 09:44:06 +0200

    Started improving & implementing MVVM code into existing projects.

--- NT.WebServer/Program.cs
+++ NT.WebServer/Program.cs
@@ -4,9 +4,9 @@ namespace NT.WebServer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
