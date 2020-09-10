f81d23124da790c0f8f3547e03e2053b1a92b055
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-10 14:20:22 +0200

    Added CallWebApi method.

--- NT.Services/OrderService.cs
+++ NT.Services/OrderService.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace NT.Services
 {
@@ -8,6 +12,43 @@ namespace NT.Services
     {
         // Class for order Services
 
+        #region CallWebApi
+        /// <summary>
+        /// Calls a given endpoint and returns a string with the retrieved json data
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        protected virtual async Task<string> CallWebApi(string url)
+        {
+            try
+            {
+                // Create a HttpWebRequest
+                HttpWebRequest httpWebRequest = WebRequest.CreateHttp(url);
+                // Set method to get
+                httpWebRequest.Method = WebRequestMethods.Http.Get;
+                // Set accept headers
+                httpWebRequest.Accept = "application/json";
 
+                // String for storing the json data retrieved from the endpoint
+                string result;
+
+                // Get response from the website
+                using(HttpWebResponse response = (HttpWebResponse)await httpWebRequest.GetResponseAsync())
+                {
+                    // Read the response
+                    using StreamReader sr = new StreamReader(response.GetResponseStream());
+                    // Assign the response data to the result variable
+                    result = await sr.ReadToEndAsync();
+                };
+
+                // Return the retrieved JSON data.
+                return result;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
     }
 }
