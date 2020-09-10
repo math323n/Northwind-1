e261106181a992399bc7fa35023d494728964b7b
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-10 13:44:41 +0200

    Added more to viewmodels

--- NT.Gui/MainWindow.xaml.cs
+++ NT.Gui/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NT.DataAccess.Repos;
+using NT.ViewModels.ViewModels;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,16 @@ namespace NT.Gui
     /// </summary>
     public partial class MainWindow: Window
     {
+       
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+
+   
+
     }
+
 }
