d0ddc1b61c28be4be38d7146e4a06f87ba2bec8d
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-11 09:35:57 +0200

    use block body suggestion

--- NT.WebApi/Program.cs
+++ NT.WebApi/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -16,11 +17,13 @@ namespace NT.WebApi
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+            {
+                 webBuilder.UseStartup<Startup>();
+            });
+        }
     }
 }
