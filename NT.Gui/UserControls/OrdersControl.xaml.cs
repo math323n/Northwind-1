fefb39192af8559621873b4f57b51b767b795aa5
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-09-11 11:26:49 +0200

    Fixed viewmodel

--- NT.Gui/UserControls/OrdersControl.xaml.cs
+++ NT.Gui/UserControls/OrdersControl.xaml.cs
@@ -1,18 +1,6 @@
-﻿using NT.DataAccess.Repos;
-using NT.ViewModels.ViewModels;
-
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NT.ViewModels.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NT.Gui.UserControls
 {
--- NT.Gui/UserControls/ProductsControl.xaml.cs
+++ NT.Gui/UserControls/ProductsControl.xaml.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NT.ViewModels.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NT.Gui.UserControls
 {
@@ -18,9 +9,18 @@ namespace NT.Gui.UserControls
     /// </summary>
     public partial class ProductsControl : UserControl
     {
+        private readonly ProductViewModel viewModel;
+
         public ProductsControl()
         {
             InitializeComponent();
+
+            viewModel = DataContext as ProductViewModel;
+        }
+
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            await viewModel.InitializeAsync();
         }
     }
 }
