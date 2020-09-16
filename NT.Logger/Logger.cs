dc713e265bbfb6b563db37f2681d96ae3fb51ea9
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-16 13:23:15 +0200

    made async

--- NT.Logger/Logger.cs
+++ NT.Logger/Logger.cs
@@ -2,25 +2,32 @@
 using System.IO;
 using System.Net.Http;
 using System.Configuration;
+using System.Threading.Tasks;
 
 namespace NT.Logger
 {
     public static class Logger
     {
+        #region Fields
         private static string logFilePath;
+        private static string logFolderPath;
+        #endregion
 
+        #region Synchronous Methods
         /// <summary>
         /// Configure the filepath.
         /// </summary>
         public static void Configure()
         {
-
-
-
+            logFolderPath = ConfigurationManager.AppSettings["LogFolderPath"].ToString();
+            // Create folder if it doesn't exist, to store logs in.
+            if(!Directory.Exists(logFolderPath))
+            {
+                // Create folder "Log"
+                _ = Directory.CreateDirectory(logFolderPath);
+            }
+            // Assign location for .txt document
             logFilePath = ConfigurationManager.AppSettings["LogFilePath"].ToString();
-
-
-            // = $"{directory}/log.txt";
         }
 
         /// <summary>
@@ -62,5 +69,41 @@ namespace NT.Logger
             // Write...
             writer.WriteLine(message);
         }
+        #endregion
+
+        #region Async Methods
+        /// <summary>
+        /// asynchronous method for handling exception Logs
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public static async Task LogAsync(Exception ex)
+        {
+            // a line of dashes to seperate logs
+            string line = new string('-', 170);
+            // The string to be written to a log
+            string message =
+                $"Date: {DateTime.Now}\n" +
+                $"Message: {ex.Message}\n" +
+                $"Source: {ex.Source}\n" +
+                $"InnerException: {ex.InnerException}\n" +
+                $"StackTrace: {ex.StackTrace}\n" +
+                $"{line}";
+            // Send message to be written
+            await WriteLogAsync(message);
+        }
+
+        /// <summary>
+        /// Write log asynchronous to the log file .txt document
+        /// </summary>
+        /// <param name="message"></param>
+        private static async Task WriteLogAsync(string message)
+        {
+           await using StreamWriter writer = new StreamWriter(logFilePath, true);
+            // Write...
+            writer.WriteLine(message);
+        }
+
+        #endregion
     }
 }
